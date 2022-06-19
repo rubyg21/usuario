d229b5f80323d90b8307e4bf4528a63cde987e7b
Rubens Galarraga <rgalarraga121@gmail.com>
2022-06-19 10:42:30 -0300

    first commit

---
+++ usuarios.components.js
@@ -0,0 +1,202 @@
+const Usuarios = {
+    model: '/usuarios',
+     API : 'https://crudcrud.com/api/d370416b0f934263afe4c75e2ce45352',
+    RAND_SIZE : 1000000000,
+
+   todos: function (cb){
+       const url = this.API + this.model
+       axios.get(url).then( response => {
+           // Me devuelve una funcion que tiene un array
+           cb(response.data)
+       }).catch( err => {
+           cb({
+               error: `${err}`
+           })
+       })
+   },
+   crear: function (obj, cb){
+       const url = this.API + this.model
+       axios.post(url, obj).then( response => {
+           cb(response.data)
+       }).catch( err => {
+           cb({
+               error: `${err}`
+           })
+       })
+   },
+   eliminar: function (id, cb){
+       const url = this.API + this.model + '/' + id
+       axios.delete(url).then( response => {
+           // Me devuelve una funcion que tiene un array
+           cb(response.data)
+       }).catch( err => {
+           cb({
+               error: `${err}`
+           })
+       })
+   },
+   editar: function (id, obj, cb){
+       const url = this.API + this.model + '/' + id
+       axios.put(url, obj).then( response => {
+           cb(response.data)
+       }).catch( err => {
+           cb({
+               error: `${err}`
+           })
+       })
+   },
+}
+
+const UsuarioComp = Vue.component('usuarios-component',{
+    data:function(){
+        return{
+            usuarios: [],
+            form: {
+                _id: null, 
+                nombre: null,
+                apellido: null,
+                clave: null,
+                sexo: null,
+                edad: null,
+                altura: null,
+                activo: null,
+                fecha: null
+            },
+            // fecha: null
+
+        }
+            
+    },
+    methods: {
+        initForm: function (){
+            this.form = {
+                _id: null, 
+                nombre: null,
+                apellido: null,
+                clave: null,
+                sexo: null,
+                edad: null,
+                altura: null,
+                activo: null,
+                fecha: this.fecha
+        
+            }
+        },
+        obtenerTodos: function (){
+            Usuarios.todos( res => {
+                this.usuarios = res
+                this.initForm()
+                this.obtenerFecha()
+                
+            })
+        },
+        guardar: function (){
+            // Crear producto
+            if(this.form._id === null){
+                const form = {...this.form}
+                delete form._id
+                Usuarios.crear(form, res => {
+                    console.log(res)
+                    this.obtenerFecha()
+                     this.obtenerTodos()
+                })
+            } else {
+                const form = {...this.form}
+                delete form._id
+                Usuarios.editar(this.form._id, form, res => {
+                    console.log(res)
+                    this.obtenerTodos()
+                })
+            }
+        },
+        eliminar: function (id){
+            Usuarios.eliminar(id, res => {
+                console.log(res)
+                this.obtenerTodos()
+                
+            })
+        },
+        editar: function (usuario){
+            this.form = {...usuario}
+        },
+        obtenerFecha: function(){
+           return this.fecha = new Date().toLocaleDateString()
+        }
+    },
+    mounted: function (){
+        this.obtenerFecha()
+        this.obtenerTodos()
+    },
+    template:`
+    <div id="usuarios--component">
+    <div id="usuarios--listado">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                    <th>Sexo</th>
+                    <th>Edad</th>
+                    <th>Altura </th>
+                    <th>Activo</th>
+                    <th>Fecha de creado</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(usuario, i) in usuarios" :key="i">
+                    <td>{{usuario._id}}</td>
+                    <td>{{usuario.nombre}}</td>
+                    <td>{{usuario.apellido}}</td>
+                    <td>{{usuario.email}}</td>
+                    <td>{{usuario.sexo}}</td>
+                    <td>{{usuario.edad}}</td>
+                    <td>{{usuario.altura}} cm</td>
+                    <td>{{usuario.activo}}</td>
+                    <td>{{usuario.fecha}}</td>
+                    <td>
+                        <button @click="editar(usuario)">Editar</button>
+                        <button @click="eliminar(usuario._id)">Eliminar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <hr>
+    <div id="usuarios--form">
+        <form action="">
+            <input type="text" placeholder="Nombre" v-model="form.nombre">
+            <br>
+            <input type="text" placeholder="Apellido" v-model="form.apellido">
+            <br>
+            <input type="password" placeholder="Clave" v-model="form.clave">
+            <br>
+            <input type="email" placeholder="Email" v-model="form.email">
+            <br>
+            Sexo:<select name="" v-model="form.sexo" >
+                <option value="masculino">Masculino</option>
+                <option value="femenino">Femenino</option>
+                <option value="otro">Otro</option>
+
+            </select>
+            <br>
+            <input type="number" placeholder="Edad" v-model="form.edad">
+            <br>
+            <input type="number" placeholder="altura" v-model="form.altura"> cm
+            <br> Activo:
+            <select name="" v-model="form.activo" placeholder="">
+                <option value="si">SI</option>
+                <option value="no">NO</option>
+
+            </select>
+                      
+            <br>
+            <button type="button" @click="guardar()">Guardar</button>
+        </form>
+        </div>
+</div>
+        
+    `
+})
